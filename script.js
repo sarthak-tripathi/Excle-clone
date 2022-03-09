f0cad135acc40a750393464edb8c46d8cc03a28e
sarthak-tripathi <89450623+sarthak-tripathi@users.noreply.github.com>
2022-03-09 22:57:28 +0530

    Outline to the cell is done when clicked

--- script.js
+++ script.js
@@ -49,11 +49,24 @@ for(let i = 0 ; i<100; i++){
 }
 
 // if i will click any cell
+// i will get the addres of that cell
+// print the address in the UI -> in the address bar
 let AllGridCell = document.querySelectorAll(".grid .cell");
 let addressInput = document.querySelector(".address_input");
 for(let i = 0; i < AllGridCell.length; i++){
 
     AllGridCell[i].addEventListener("click",function(e){
+        //previous cell address
+        let  prevaddress =addressInput.value;
+        if(prevaddress != ""){
+            let ridcidObj = getRidCidFromAddress(prevaddress);
+            // alert(ridcidObj.rid + "" + ridcidObj.cid);
+            // previous-> rremove border
+            let prevCell = document
+                .querySelector(`.grid .cell[rid='${ridcidObj.rid}'][cid='${ridcidObj.cid}']`);
+            prevCell.style.border = "0.5px solid gray";
+        }
+        
         
             let rid = AllGridCell[i].getAttribute("rId");
             let cid = AllGridCell[i].getAttribute("cID");
@@ -62,14 +75,34 @@ for(let i = 0; i < AllGridCell.length; i++){
             cid = Number(cid);
             // alert(String.fromCharCode(cid +65) + " " + (rid + 1));
 
-    //     alert("Bro yu can do it...Let's go!!!!");
-    // String.fromCharCode() method converts Unicode values to characters.
+            // alert("Bro yu can do it...Let's go!!!!");
+            // String.fromCharCode() method converts Unicode values to characters.
             addressInput.value = String.fromCharCode(cid +65) +  (rid + 1);
+
+            //current cell styling add
+
+            let cCell = document.querySelector(`.grid .cell[rid='${rid}'][cid='${cid}']`);
+            cCell.style.border = "2px solid green";
     })
 
 }
-// i will get the addres of that cell
-// print the address in the UI -> in the address bar
+
+// first cell select
+
+let firstCell = document.querySelector(".grid .cell[rid = '0'][cid = '0']");
+firstCell.click()
+firstCell.style.border = "2px solid green";
+
+function getRidCidFromAddress(address){
+    //A->Z ,1-100
+    // A20
+    let AsciiValue = address.charCodeAt(0);
+    let cid =AsciiValue - 65;
+    let rid = Number(address.substring(1))- 1;
+    return{
+        rid :rid, cid : cid
+    }
+}
 
 
 
