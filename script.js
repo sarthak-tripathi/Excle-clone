8bcac97b5a156abc9fe33668b79d7a7bf2a33d77
sarthak-tripathi <89450623+sarthak-tripathi@users.noreply.github.com>
2022-03-09 13:12:02 +0530

    address is in the addressBar Done

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ for(let i =0; i< 26; i++){
 }
 let leftCol = document.querySelector(".left_col");
 
-for(let i =0; i<=100; i++){
+for(let i =1; i<=100; i++){
     let div = document.createElement("div");
     div.setAttribute("class","cell");
     div.textContent =i;
@@ -40,10 +40,36 @@ for(let i = 0 ; i<100; i++){
 
         div.setAttribute("contentEditable","true");
         // every cell identification is required.
-        div.setAttribute("rid",i);
-        div.setAttribute("cid",j);
+        div.setAttribute("rId",i);
+        div.setAttribute("cId",j);
         row.appendChild(div);
 
     }
     grid.appendChild(row);
 }
+
+// if i will click any cell
+let AllGridCell = document.querySelectorAll(".grid .cell");
+let addressInput = document.querySelector(".address_input");
+for(let i = 0; i < AllGridCell.length; i++){
+
+    AllGridCell[i].addEventListener("click",function(e){
+        
+            let rid = AllGridCell[i].getAttribute("rId");
+            let cid = AllGridCell[i].getAttribute("cID");
+            // alert(rid+" "+cid);
+            rid = Number(rid);
+            cid = Number(cid);
+            // alert(String.fromCharCode(cid +65) + " " + (rid + 1));
+
+    //     alert("Bro yu can do it...Let's go!!!!");
+    // String.fromCharCode() method converts Unicode values to characters.
+            addressInput.value = String.fromCharCode(cid +65) +  (rid + 1);
+    })
+
+}
+// i will get the addres of that cell
+// print the address in the UI -> in the address bar
+
+
+
